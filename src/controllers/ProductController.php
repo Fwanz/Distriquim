ce0f61f6d916666983331e0790724c325c0fb52a
Ellery <esaulagj@gmail.com>
2019-01-30 22:09:46 -0600

    Added get method. You can write queries

--- src/controllers/ProductController.php
+++ src/controllers/ProductController.php
@@ -1,38 +1,59 @@
 <?php
 
-  namespace Controllers;
+namespace Controllers;
 
-  use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\Query\QueryException;
 
 
-  /**
-   *
-   */
-  class ProductController
-  {
+/**
+ *
+ */
+class ProductController
+{
 
     private $productRepository;
     private $entityManager;
+    private $queryBuilder;
 
     function __construct(EntityManager $em)
     {
-      $this->entityManager = $em;
-      $this->productRepository = $this->entityManager->getRepository('Product');
+        $this->entityManager = $em;
+        $this->queryBuilder = $em->createQueryBuilder();
+        $this->productRepository = $this->entityManager->getRepository('Product');
     }
 
     public function findAll()
     {
-       return $this->productRepository->findAll();
+        return $this->productRepository->findAll();
     }
 
-    public function findById(int $id){
-      return $this->productRepository->find($id);
+    public function findById(int $id)
+    {
+        return $this->productRepository->find($id);
     }
 
     public function findBy(array $params)
     {
-      return $this->productRepository->findBy($params);
+        return $this->productRepository->findBy($params);
     }
-  }
 
- ?>
+    public function get($attrs, $condition, $distinct = FALSE, $orderBy = NULL, $direction = 'DESC')
+    {
+        try {
+            $this->queryBuilder->select($attrs)->from('Product', 'p')->where($condition);
+            if ($distinct) {
+                $this->queryBuilder->distinct(true);
+            }
+            if ($orderBy) {
+                $this->queryBuilder->orderBy($orderBy, $direction);
+            }
+            $query = $this->queryBuilder->getQuery();
+            return $query->getResult();
+        } catch (QueryException $e) {
+            return $e->getMessage();
+        }
+    }
+}
+
+?>
--- test.php
+++ test.php
@@ -1,36 +1,27 @@
-<?php 
-	include './src/EntityContainer.php';
-	$allProducts = $productController->findAll();
-	$productsByKey = $productController->findBy(['brand' => 'KRONOS']);
- ?>
+<?php
+include './src/EntityContainer.php';
+$urlParts = parse_url($_SERVER['REQUEST_URI']);
+// $marca = str_replace("/distriquim/", "", $urlParts['path']);
+$marca = 'KRONOS';
+$productsByCategory = $productController->get("p.category", "p.brand = '$marca'", true);
+?>
 <!DOCTYPE html>
 <html>
 <head>
-	<title></title>
-	<style>
-		.findAll, .findByExample{
-			width: 50%;
-		}
-	</style>
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+    <title>Testing Doctrine</title>
 </head>
 <body>
-	<div class="findAll">
-		<h2>This is all products</h2>
-		<?php 
-			foreach ($allProducts as $product) {
-	    		echo $product->getSecuritySheet() . "<br>";
-			}
- 		?>
-	</div>
-	<div class="findByExample">
-		<h2>Example of a query with params. Just pass what you want as an array</h2>
-		<?php 
-			foreach ($productsByKey as $product) {
-	    		echo $product->getName() . "<br>";
-	    		echo $product->getTechnicalSheet() . "<br>";
-	    		echo $product->getSecuritySheet() . "<br><br>";
-			}
- 		?>
-	</div>
+<div class="findByExample">
+    <h2>Example of a query with params. Just pass what you want as an array</h2>
+    <ul>
+        <?php
+        foreach ($productsByCategory as $product) {
+            echo utf8_encode($product['category']);
+        };
+        ?>
+    </ul>
+</div>
 </body>
 </html>
